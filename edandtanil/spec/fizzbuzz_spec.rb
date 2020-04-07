a0ef1937c14d09a8c3b36037a52c75447aaf186e
Ed <lookatemail@gmail.com>
2020-04-07 10:44:38 +0100

    edand tanil 03

--- edandtanil/spec/fizzbuzz_spec.rb
+++ edandtanil/spec/fizzbuzz_spec.rb
@@ -1,12 +1,16 @@
+require 'fizzbuzz'
 
 describe 'Fizzbuzz_ET' do
 
   it 'should retun 1 if 1 passed in' do
     expect(fizzbuzz(1)).to eq 1
   end
-  it 'should return error if -1 passed in'
+
+  it 'should return error if -1 passed in' do 
+
+  end
 
 end
 
 # rspec ./edandtanil
-#rspec spec/fizzbuzz_Spec.rb
+#rspec spec/fizzbuzz_spec.rb
