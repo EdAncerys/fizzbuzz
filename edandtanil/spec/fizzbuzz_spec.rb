ab0901154bdd8164546d7e833987f70c3350a03e
tanilserbes <serbestanil@gmail.com>
2020-04-07 10:38:52 +0100

    added line in spec_file

--- edandtanil/spec/fizzbuzz_spec.rb
+++ edandtanil/spec/fizzbuzz_spec.rb
@@ -1,10 +1,11 @@
 
 describe 'Fizzbuzz_ET' do
 
-  it 'should retun 1 if 1 passed in' do 
-
+  it 'should retun 1 if 1 passed in' do
+    expect(fizzbuzz(1)).to eq 1
   end
 
 end
 
 # rspec ./edandtanil
+#rspec spec/fizzbuzz_Spec.rb
