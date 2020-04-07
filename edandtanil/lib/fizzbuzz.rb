c9596522fa9780e57bfbd05ba321b9709ce5ee8c
Ed <lookatemail@gmail.com>
2020-04-07 11:15:06 +0100

    test 5 added

--- edandtanil/lib/fizzbuzz.rb
+++ edandtanil/lib/fizzbuzz.rb
@@ -1,13 +1,14 @@
 
 def fizzbuzz (number)
-if number % 3 == 0
-  "fizz"
-elsif number % 5 == 0
-  "buzz"
-else
-  number
-end
-# fizz = number % 3 == 0
-# buzz = number % 5 == 0
+
+  if number % 3 == 0 && number % 5 == 0
+    "fizzbuzz"
+  elsif number % 3 == 0
+    "fizz"
+  elsif number % 5 == 0
+    "buzz"
+  else
+    number
+  end
 
 end
--- edandtanil/spec/fizzbuzz_spec.rb
+++ edandtanil/spec/fizzbuzz_spec.rb
@@ -12,8 +12,17 @@ describe 'Fizzbuzz_ET' do
 
   it 'should return error if -1 passed in' do
     expect(fizzbuzz(15)). to eq 'fizzbuzz'
-end
+  end
+
+  it 'should return buzz if 5 passed in' do
+    expect(fizzbuzz(5)).to eq "buzz"
+  end
+
+  it 'should return "wrong input" if 0 passed in' do 
+    expect(fizzbuzz(0)).to eq "wrong input"
+  end
 
 end
+
 # rspec ./edandtanil
 #rspec spec/fizzbuzz_spec.rb
